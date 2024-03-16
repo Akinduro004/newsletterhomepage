ca4626f8ec4beb4f86acc77f00c0ce5f4ca058df
Akinduro Folake <159786865+Akinduro004@users.noreply.github.com>
2024-03-16 10:28:11 +0100

    Add files via upload

---
+++ newshomepage.js
@@ -0,0 +1,7 @@
+function enableLightMode() {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+  
+  function enableDarkMode() {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
